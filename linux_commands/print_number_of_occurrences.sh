6f157f66cf56ef7c9568aaa59389f35ca9f24851
CrioUser <crio-user@criodo.com>
2020-08-27 14:49:16 +0530

    ME_QBOX_MODULE_logins_COMMAND

--- linux_commands/print_number_of_occurrences.sh
+++ linux_commands/print_number_of_occurrences.sh
@@ -8,5 +8,5 @@
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
     # 3) Execute the script before submission and confirm the output
 
-
+cat ~/workspace/QBox/logs/vsftpd1.log | grep "230 Login successful" | wc -l
 
